94ed4d6cc7e4246ce01edb694d839ed8a67e2318
Jordan Johnson <jordaninjohnson@gmail.com>
2020-07-15 22:23:19 -0600

    Search works great

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import API from "./utils/API";
 import Table from "./components/Table"
-import Form from './components/Form'
-
 
 class App extends Component {
     state = {
         results: [],
+        filteredResults: [],
+        name: "",
     };
 
     componentDidMount() {
@@ -16,17 +16,34 @@ class App extends Component {
     searchAPI = () => {
         API.fetch()
             // .then(res => console.log(res))
-            .then(res => this.setState({ results: res.data.results }))
+            .then(res => this.setState({ ...this.state, results: res.data.results, filteredResults: res.data.results }))
             .catch(err => console.log(err));
     };
 
+    setName = (e) => {
+        let tempResults = this.state.results.filter(emp => { return emp.name.first.includes(e) || emp.name.last.includes(e)} )
+        this.setState({...this.state, name:e, filteredResults: tempResults});
+        // console.log(this.state.filteredResults);
+
+    }
+
     render() {
-        const { results } = this.state
+        const { filteredResults } = this.state
 
         return (
             <div className="container">
-                <Form />
-                <Table resultData={results}/>
+                <form>
+                    <label htmlFor="name">Search:</label>
+                    <span> </span>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Enter Name"
+                        value={this.state.name}
+                        onChange={e => this.setName(e.target.value)} />
+                </form>
+                <Table resultData={filteredResults} />
             </div>
         )
     }
