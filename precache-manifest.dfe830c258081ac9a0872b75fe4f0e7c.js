0387a7227e5bbd7a9599d721505aebb183a2b359
Jordan Johnson <jordaninjohnson@gmail.com>
2020-08-05 17:07:10 -0600

    Updates

---
+++ precache-manifest.dfe830c258081ac9a0872b75fe4f0e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b067275230d5653fa2224bfae76b2aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9dcdce6c8895e03c60",
+    "url": "/static/js/2.aec48305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aec48305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19863694d4635d4d874",
+    "url": "/static/js/main.523e3cff.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
